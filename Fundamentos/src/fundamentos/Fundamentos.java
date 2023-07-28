491e1801f77451cc3ace3c61cd885da648792cba
gomessenai <guilherme.ferreira3@aluno.senai.br>
2023-07-28 10:48:37 -0300

    Criando variavei em java

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -14,7 +14,13 @@ public class Fundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Declaração de variaveis
+        
+        String nome = "Gomes";
+        int idade = 16;
+        boolean estudando = true;
+        double media = 8.8;
     }
+    
     
 }
