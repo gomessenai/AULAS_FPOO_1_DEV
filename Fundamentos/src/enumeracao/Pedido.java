19eab1b1d167df7d4e04c5c652aec08b5199d16f
gomessenai <guilhereme.ferreira3@aluno.senai.br>
2023-08-17 10:18:51 -0300

    Aula de Enumeração em Java

---
+++ Fundamentos/src/enumeracao/Pedido.java
@@ -0,0 +1,30 @@
+
+package enumeracao;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Pedido {
+    private String descricao;
+    private double valor;
+    private String status;
+
+    public Pedido(String descricao, double valor, String status) {
+        this.descricao = descricao;
+        this.valor = valor;
+        this.status = status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" + "descricao=" + descricao + ", valor=" + valor + ", status=" + status + '}';
+    }
+    
+    
+    
+}
---
+++ Fundamentos/src/enumeracao/StatusEnum.java
@@ -0,0 +1,23 @@
+
+package enumeracao;
+
+/**
+ *
+ * @author Aluno
+ */
+public enum StatusEnum {
+    EM_PREPARACAO("Em preoparação"),
+    EM_TRANSPORTE("Em transporte"),
+    ENTREGUE("Entregue ao cliente");
+    
+    private String status;
+    
+    private StatusEnum(String status) {
+        this.status = status;
+    }
+    public String getStatus (){
+        return this.status;
+    }
+    
+    
+}
